6a728360994018e3429f7c6d4b09c2f46b5dc128
DarkNoriss <darknoriss@gmail.com>
2023-03-23 01:31:57 +0100

    working on modal

--- src/functions/createModal.js
+++ src/functions/createModal.js
@@ -4,13 +4,38 @@ export const getModal = () => {
 };
 
 const createModal = () => {
-  const body = document.querySelector("body");
   const modal = document.createElement("dialog");
   modal.classList.add("modal");
+
   const form = document.createElement("form");
   form.classList.add("form-task");
   form.method = "dialog";
+
+  const header = document.createElement("h2");
+  header.innerText = "Creating task";
+  form.append(header);
+
+  const inputName = document.createElement("input");
+  inputName.placeholder = "Name";
+  inputName.type = "text";
+  inputName.name = "task-name";
+  form.append(inputName);
+
+  const inputDesc = document.createElement("textarea");
+  inputDesc.placeholder = "Description";
+  inputDesc.name = "task-desc";
+  inputDesc.rows = 4;
+  inputDesc.cols = 50;
+  form.append(inputDesc);
+
+  const btnSub = document.createElement("button");
+  btnSub.innerText = "Create";
+  form.append(btnSub);
+
   modal.append(form);
+
+  const body = document.querySelector("body");
   body.append(modal);
+
   return modal;
 };
